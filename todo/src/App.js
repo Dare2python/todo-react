3be783b014d19f4a30459cda1dea058a5d8ca25a
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-02 15:23:19 +0300

    change to Component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h4 className="bg-primary text-white text-center p-2">
-        To Do list
-      </h4>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h4 className="bg-primary text-white text-center p-2">
+          To Do list
+        </h4>
+      </div>
+    );
+  };
+
+
 }
-
-export default App;
