26685c79f78703978caee910d1b0b069235d9f2f
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-02 15:26:22 +0300

    Adding State Data and Data Bindings

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: "Adam"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h4 className="bg-primary text-white text-center p-2">
-          To Do list
+          {this.state.userName} To Do list
         </h4>
       </div>
     );
