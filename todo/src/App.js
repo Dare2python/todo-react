8f3182cb2c448051fa12ebfe6990bcfa1e4304c5
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-02 15:36:16 +0300

    Changing State Data

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,12 +9,24 @@ export default class App extends Component {
     }
   }
 
+  changeStateData = () => {
+    this.setState(
+      {
+        userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName} To Do list
+          {this.state.userName}'s To Do list
         </h4>
+        <button className="btn btn-primary m-2"
+        onClick={this.changeStateData}>
+          Change
+        </button>
       </div>
     );
   };
